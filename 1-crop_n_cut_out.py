4bf9427898321aa6de7388ebbd82d15a7a76c4d4
ErykDarnowski <github@erykdarnowski.pl>
2023-10-20 12:15:38 +0200

    Add further explanations and more debug info

--- 1-crop_n_cut_out.py
+++ 1-crop_n_cut_out.py
@@ -2,10 +2,9 @@
 # https://stackoverflow.com/a/59669649/11749019
 # https://stackoverflow.com/questions/61497956/cut-out-a-specific-part-of-an-image-with-opencv-in-python
 
-"""TODO
+""" TODO
 - higher resolution mask for cleaner output
 - better solution for the transparency?
-- figure out and comment what the transparency conversion does
 """
 
 import cv2
@@ -15,18 +14,23 @@ img_org = cv2.imread('input.jpg')
 img_mask = cv2.imread('mask/mask.jpg')
 
 # crop out disc from img
-size = 1067 # basically the size of the disc from left edge to where it ends - do this by edge / color diff detection? - 1200 - 133 = 1067 (instead of 5645)
+size = 1067 # basically the size (amount of px) of the disc from the left edge to where it ends - do this by edge / color diff detection? - 1200 - 133 = 1067 (instead of 5645)
 img_org_cropped = img_org[0:size, 0:size]
 
 # apply mask to image (cut out) -> <https://stackoverflow.com/questions/44333605/what-does-bitwise-and-operator-exactly-do-in-opencv>
 img_cut = cv2.bitwise_and(img_mask, img_org_cropped)
 
 # change black color to transparency -> <https://stackoverflow.com/questions/40527769/removing-black-background-and-make-transparent-from-grabcut-output-in-python-ope>
-tmp = cv2.cvtColor(img_cut, cv2.COLOR_BGR2GRAY)
-_, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
-b, g, r = cv2.split(img_cut)
+tmp = cv2.cvtColor(img_cut, cv2.COLOR_BGR2GRAY) # change to grayscale
+_, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY) # find black pixels -> <https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html>
+b, g, r = cv2.split(img_cut) # split into single-channel images (based on color) -> <https://www.geeksforgeeks.org/splitting-and-merging-channels-with-python-opencv/>
 rgba = [b, g, r, alpha]
-result = cv2.merge(rgba, 4)
+result = cv2.merge(rgba, 4) # add in the alpha channel (transparency)
 
 # save result
 cv2.imwrite('circle.png', result)
+
+""" debug
+cv2.imshow('circle', tmp)
+cv2.waitKey(0)
+"""
--- 2-center.py
+++ 2-center.py
@@ -21,7 +21,7 @@ args = parser.parse_args()
 
 MIN_MATCH_COUNT = 2
 
-# hardcoded but goofy
+# hardcoded
 debug_filename = 'debug.jpg'
 output_filename = 'output.png'
 
@@ -46,6 +46,15 @@ def get_matched_coordinates(temp_img, map_img):
 
     # initiate SIFT (Scale-Invariant Feature Transform) detector -> <https://docs.opencv.org/4.x/da/df5/tutorial_py_sift_intro.html>
     sift = cv2.SIFT_create()
+    """
+    The SIFT (Scale-Invariant Feature Transform) algorithm, developed by D.Lowe
+    in 2004, is used for detecting and describing local features in images. It
+    is scale-invariant, meaning it can find the same features even if the image
+    is scaled.
+    
+    The algorithm is widely used in computer vision tasks like object detection,
+    image recognition, and more.
+    """
 
     # find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(temp_img, None)
@@ -74,53 +83,79 @@ def get_matched_coordinates(temp_img, map_img):
         # find homography -> <https://en.wikipedia.org/wiki/Homography_(computer_vision)> / <https://en.wikipedia.org/wiki/Homography>
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         matchesMask = mask.ravel().tolist()
+        """
+        In the field of computer vision, a homography is a transformation that maps
+        any two images of the same planar surface in space, assuming a pinhole camera
+        model. This has practical applications such as image rectification, image
+        registration, or determining camera motion—rotation and translation—between
+        two images.
+
+        In projective geometry, a homography is an isomorphism of projective spaces,
+        induced by an isomorphism of the vector spaces from which the projective spaces
+        derive. It is a bijection that maps lines to lines, and thus a collineation.
+        This concept was introduced to understand, explain and study visual perspective,
+        and the difference in appearance of two plane objects viewed from different points
+        of view.
+        """
 
         h, w = temp_img.shape
         pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
         dst = cv2.perspectiveTransform(pts, M) # matched coordinates
 
 
-        ## START
+        ## START (my mod)
+        cords = np.int32(dst)
         rect = cv2.minAreaRect(dst)
-        test = np.int32(dst)
 
         # go through all coordinates to find index of the highest point
-        for i in range(len(test)):
-            print(test[i])
-            if (test[i][0][1] == test.min(axis=0)[0][-1]): # match to highest point (lowest `y`)
+        print("[DEBUG - template info]\n  cords:")
+
+        for i in range(len(cords)):
+            print(f"    {cords[i]}")
+            if (cords[i][0][1] == cords.min(axis=0)[0][-1]): # match to highest point (lowest `y`) -> <https://stackoverflow.com/questions/35930617/how-to-find-minimum-maximum-values-axis-by-axis-in-numpy-array>
+                highest_point_cords = cords[i][0]
                 orientation = i
 
-        # get found template's angle (by using cv2's approach to angles in `minAreaRect`)
+        print("  rect:")
+        for i in rect:
+            if type(i) is tuple:
+                print(f"    {round(i[0])}, {round(i[1])}")
+            else:
+                print(f"    {round(i)}")
+
+        """
+        - better approach to margin and string interpolation?
+        - what about name of rect debug
+        """
+
+        # get found template's angle -> <https://theailearner.com/tag/cv2-minarearect/>
         angle = rect[-1]
 
-        # add offset to angle to correct the disc's rotation
-        print(f"\norientation: {orientation}")
-        print(f"detected {round(angle)} deg")
-        angle += orientation * 90
-        print(f"corrected: {round(angle)} deg")
+        # add offset to the angle to correct the disc's rotation
+        print(f"\n  orientation: {orientation} ({orientation * 90}°)")
+        print(f"  detected {round(angle)}°")
+        angle += orientation * 90 # angle is the amound of degrees the image needs to be roated (counter-clockwise) or by doing (360 - angle) clockwise
+        print(f"  corrected: {round(angle)}°")
 
-        # load unchanged map image
+        # load og map image
         circle = cv2.imread(args.map, cv2.IMREAD_UNCHANGED)
 
         # center map image 
         rotated_image = ndimage.rotate(circle, angle, reshape=False)
 
-        """
-        # using `cv2.getRotationMatrix2D` to get the rotation matrix
-        rotate_matrix = cv2.getRotationMatrix2D((circle.shape[1] / 2, circle.shape[0] / 2), angle, 1)
-        #                            center pos ^                              angle in deg ^      ^ scale
-        # rotating the image
-        rotated_image = cv2.warpAffine(circle, rotate_matrix, (circle.shape[1], circle.shape[0]))
-        """
-        # ^ CAN'T BE USED AS THE RESULT IS BLURRY AND UGLY
-
-        # writing centered image
+        # write corrected (centered) image
         cv2.imwrite(os.path.join(output_filename), rotated_image)
         ## END
 
 
-        # drawing rectangle around found template on map image
-        map_img = cv2.polylines(map_img, [test[0], test[1], test[2]], True, 255, 3, cv2.LINE_AA)
+        # drawing highest point and rectangle around found template on map image
+        map_img = cv2.circle(map_img, highest_point_cords, radius=25, color=(0, 0, 255), thickness=8)
+        map_img = cv2.polylines(map_img, [cords[0], cords[1], cords[2]], True, 255, 3, cv2.LINE_AA)
+
+        """ debug
+        cv2.imshow('circle', map_img)
+        cv2.waitKey(0)
+        """
     else:
         print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
         matchesMask = None
@@ -138,8 +173,11 @@ def get_matched_coordinates(temp_img, map_img):
     # result image path
     if (args.save_dir != '' and args.save_dir != './'):
         #                          ubuntu fix ^
+        
+        # dir cleanup
         if not os.path.exists(args.save_dir):
             os.mkdir(args.save_dir)
+
         cv2.imwrite(os.path.join(args.save_dir, debug_filename), img3)
     else:
         cv2.imwrite(os.path.join(debug_filename), img3)
